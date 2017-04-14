4cd628a279f65c7cc378a31e7f4278197fcf26a4
Omri Bernstein <hippiccolo@gmail.com>
2017-04-14 11:20:55 -0400

    Stop sharing all project files

--- server/app/statics.middleware.js
+++ server/app/statics.middleware.js
@@ -9,7 +9,6 @@ var browserPath = path.join(rootPath, 'browser');
 var buildPath = path.join(rootPath, 'build');
 var nodeModulesPath = path.join(rootPath, 'node_modules');
 
-router.use(express.static(rootPath));
 router.use(express.static(browserPath));
 router.use(express.static(buildPath));
 router.use(express.static(nodeModulesPath));
